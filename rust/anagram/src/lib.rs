ca197d3fcee698732b273d3f71547eaba3986814
kodnova <Dev@KodNova.dev>
2026-06-23 02:33:54 -0500

    anagram: progress

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,5 +1,18 @@
+use core::iter::Iterator;
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let sorted_word = sort_word(word);
+    let answers: HashSet<&str> = possible_anagrams
+        .iter()
+        .copied()
+        .filter(|pos| (word != *pos) && (sorted_word == sort_word(pos)))
+        .collect();
+    answers
+}
+
+pub fn sort_word(word: &str) -> Vec<char> {
+    let mut chars: Vec<char> = word.chars().collect();
+    chars.sort();
+    chars
 }
